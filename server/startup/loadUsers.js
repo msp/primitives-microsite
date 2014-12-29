36e5e6b65ffa396e9f309a5b587edfd533df219a
msp <matt@cenatus.org>
2014-12-29 16:29:04 +0000

    Upgrade Meteor to 1.0.2.1. I also regenerated the seeded users so keep the debug in for that.

--- server/startup/loadUsers.js
+++ server/startup/loadUsers.js
@@ -3,13 +3,18 @@ function loadUser(user) {
 
   if (!userAlreadyExists) {
     Accounts.createUser(user);
+  } else {
+    console.log("DUPE! "+user.username);
   }
 }
 
 Meteor.startup(function () {
   var users = YAML.eval(Assets.getText('users.yml'));
 
+  console.log("Starting user load...");
   for (key in users) if (users.hasOwnProperty(key)) {
+    //console.log("loading.. users[key]: "+users[key].username);
     loadUser(users[key]);
   }
+  console.log("DONE user load!");
 });
